443fb5ccc95edc19bd81b7e5a99c8ba0f5e7a3d9
davagaluhd <davagaluh22@gmail.com>
2023-02-05 22:15:24 +0700

    commit-git

---
+++ Soal-1.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const nilai = prompt('Masukan nilai: ');
+var hasil;
+
+if (nilai%2==0) {
+    hasil = Math.sqrt(nilai);
+    console.log("Hasil akar pangkat 2 dari", + nilai, "adalah =", + hasil)
+}
+else if (nilai <= 0){
+    console.log("Tidak bisa input bilangan negatif")
+} else{
+    console.log("Tidak bisa input bilangan ganjil")
+}
